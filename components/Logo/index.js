1d8f9e3cbec674a7885e98ee982beed936e69126
Matt Derocher <mjamesderocher@gmail.com>
2021-02-15 08:03:53 -0500

    Added logo

--- components/Logo/index.js
+++ components/Logo/index.js
@@ -1,9 +1,10 @@
 
 import React from 'react' 
+import './global.css?raw'
 
 const Logo = () => {
   return (
-    <svg id="svg65" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 96.46" width="70" fill="currentColor">
+    <svg id="svg65" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 96.46" fill="currentColor">
       <path d="M99.91,85.69,64.1,86,64,76.64H90.67l.12-67.14-36.35,0V.3L100,.27l-.09,85.42Z" transform="translate(0 -0.27)"/>
       <path d="M14.64.41H51.7V9.58l-26.58.05.05,13.92,22.22.06,0,8.51-22.21-.06c.49,10.16.22,30.62.22,30.62l-10.92,0Z" transform="translate(0 -0.27)"/>
       <path d="M50.56,84.77,50.4,23.21l37.06-.13,0,9.17-26.58.15L61,46.31l22.21,0,.08,8.51-22.21,0,.11,30.83-.05.55-10.48,0Z" transform="translate(0 -0.27)"/>
