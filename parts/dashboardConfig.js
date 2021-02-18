d11afeb7bc70daab54eb096a7191a2dcc015137b
Matt Derocher <mjamesderocher@gmail.com>
2021-02-18 07:20:37 -0500

    Fixed build hook for Netlify

--- parts/dashboardConfig.js
+++ parts/dashboardConfig.js
@@ -8,7 +8,7 @@ export default {
           {
             title: 'The Former Faith',
             apiId: '181a92ff-510b-4d55-9dbf-bb762a3b821a',
-            buildHookId: '5fb0834f5a29022cb032e475',
+            buildHookId: '602e5b637d184d3e4dcc5745',
             name: 'former-faith'
           }
         ]
